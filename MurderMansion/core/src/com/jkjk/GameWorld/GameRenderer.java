b61b012bf85201998546ee4620a064101f4376f3
unknown <jxlee92@gmail.com>
2015-03-18 23:02:48 +0800

    Changes light range to 250 *no need to change collision cone Updated some parts of the map

--- MurderMansion/core/src/com/jkjk/GameWorld/GameRenderer.java
+++ MurderMansion/core/src/com/jkjk/GameWorld/GameRenderer.java
@@ -82,7 +82,7 @@ public class GameRenderer {
 		// Create Light for player
 		rayHandler = new RayHandler(gWorld.getWorld());
 		rayHandler.setAmbientLight(0.1f);
-		coneLight = new ConeLight(rayHandler, 100, null, 200, 0, 0, 0, 40);
+		coneLight = new ConeLight(rayHandler, 100, null, 250, 0, 0, 0, 40);
 		coneLight.attachToBody(player.getBody(), -10, 0);
 		ConeLight.setContactFilter((short) 2, (short) 2, (short) 1);
 
